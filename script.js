474b6b27da0c3ec831523767b8aab32fb660831d
anointedgraphic1-source <anointedgraphic1@gmail.com>
2026-08-03 09:42:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+// Mobile Menu
 const menu = document.querySelector(".menu-toggle");
 const nav = document.querySelector(".nav-links");
 
 menu.addEventListener("click", () => {
     nav.classList.toggle("active");
+});
+
+// Sticky Navbar
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
 });
